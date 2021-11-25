c623bc5939851e2a5bcb6c43d85deae1f7521829
Amna Bashir <amna.bashir2@gmail.com>
2021-11-25 10:50:26 +0000

    lab hw hannah

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -10,10 +10,9 @@ class TestPub(unittest.TestCase):
     def test_pub_has_name(self):
         self.assertEqual("The parncing pony", self.pub.name)
 
-    def test_pub_increase_money(self):
-        increase_money= 
-        # self.assertEqual(100, self.pub.cash)
-        assert increase_money = 100
+    def test_pub_increase_money(self, amount):
+        self.pub.increase_ += 
+        
 
     def test_recieve_drink(self, reduce_stock):
         reduce_stock== -1
